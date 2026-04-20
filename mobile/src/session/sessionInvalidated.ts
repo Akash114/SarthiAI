fcb4b76ec54c0b5f832136a3de49f6c0165ed838
5kyguy <0x5kyguy@gmail.com>
2026-04-20 16:56:19 +0530

    chore: restart from scratch, delete most previous code and files

--- mobile/src/session/sessionInvalidated.ts
+++
@@ -1,9 +0,0 @@
-let onInvalidate: (() => void) | null = null;
-
-export function setSessionInvalidateHandler(fn: (() => void) | null): void {
-  onInvalidate = fn;
-}
-
-export function notifySessionInvalidated(): void {
-  onInvalidate?.();
-}
